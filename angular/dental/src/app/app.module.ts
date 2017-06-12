00513524d4329cbe91c338552219d88537349959
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2017-06-12 19:21:58 -0300

    Adicionando CSS do Materialize

--- angular/dental/src/app/app.module.ts
+++ angular/dental/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule
+    ,MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
