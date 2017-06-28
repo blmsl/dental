16e2349e815b841893bc3c0ad6b91ffe59496973
Jonathan <jonathanbruno.ueg@gmail.com>
2017-06-28 19:51:29 -0300

    Create schedule model class

---
+++ angular/dental/src/app/schedule/schedule/shared/schedule.ts
@@ -0,0 +1,11 @@
+export class Schedule{
+    id:number;
+    start:Date;
+    end:Date;
+    title:string;
+    patient_id:number;
+    dentist_id:number;
+    estimated_time:number;
+    schedule_time:Date;
+    observation:string;
+}
